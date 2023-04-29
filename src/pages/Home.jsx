3d3c344babdc87d821f523afb5838a5e4e6fb6b2
BahodirDev <qurbonovbahodirjon6969@gmail.com>
2023-04-30 01:34:56 +0500

    Abdulaziz

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,24 +9,19 @@ function Home() {
                 <div className='home_div2'>
                     <div className='button_div'>
                         <Link to={"/Firmware/"} className='button_div2'>
-                             
-                            <h5>01. Fimware /</h5>
+                            <h5>01. Firmware</h5>
                         </Link>
                         <Link to={"/Software/"} className='button_div2'>
-                             
-                            <h5>02. Software/ </h5>
+                            <h5>02. Software</h5>
                         </Link>
                         <Link to={"/Documents/"} className='button_div2'>
-                             
-                            <h5>03. Documents/ </h5>
+                            <h5>03. Documents</h5>
                         </Link>
                         <Link to={"/Temperature Screening/"} className='button_div2'>
-                             
-                            <h5>04. Temperature <tr></tr> Screening/   </h5>
+                            <h5>04. Temperature Screening</h5>
                         </Link>
                         <Link to={'/VMS/'} className='button_div2'>
-                             
-                            <h5>5. VMS/   </h5>
+                            <h5>5. VMS</h5>
                         </Link>
                     </div>
                 </div>
